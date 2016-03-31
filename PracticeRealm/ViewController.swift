8901b0e739b4a7aa3cc6e58f40ad3939ec68447a
peoplewhite <shonshon7@gmail.com>
2016-03-31 12:45:41 +0800

    Initial Commit

---
+++ PracticeRealm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeRealm
+//
+//  Created by sean on 2016/3/31.
+//  Copyright © 2016年 oddesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
